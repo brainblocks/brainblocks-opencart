c0f8b97f20f052551d2302e2d13e82934928676b
David Teal <david.teal@uk.aql.com>
2019-05-26 19:05:41 +0100

    Fix token re-check bug
    For tokens longer than 255 characters

--- upload/admin/model/extension/payment/brainblocks.php
+++ upload/admin/model/extension/payment/brainblocks.php
@@ -7,7 +7,7 @@ class ModelExtensionPaymentBrainblocks extends Model
         $this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "brainblocks_tokens` (
             `brainblocks_tokens_id` int(11) NOT NULL AUTO_INCREMENT,
             `order_id` int(11) NOT NULL DEFAULT '0',
-            `token` VARCHAR(255) NOT NULL DEFAULT '',
+            `token` text NOT NULL DEFAULT '',
             `date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
             PRIMARY KEY (`brainblocks_tokens_id`)
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
